29628048530580a3120048e422c7df4c5e5d381c
saso78 <sasotrpovski@gmail.com>
2018-12-17 15:00:29 +0100

    added folders container and components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+const Logo = () => {
+	return (
+	<div className='ma3 mt0'>
+	<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+ <div className="Tilt-inner"> 👽 </div>
+</Tilt>
+	</div>
+
+		)
+}
+
+
+
+export default Logo;
