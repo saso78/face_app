1873d76e6c78f411cb158056c4efc6d021e70382
saso78 <sasotrpovski@gmail.com>
2018-12-18 23:23:02 +0100

    added all components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Rank = () => {
+return (
+  <div >
+    <div className='f3 white '>
+      {'Saso our rank is...'}
+    </div>
+    <div className='f1 white '>
+      {'## 5'}
+    </div>
+  </div>
+
+		)
+}
+
+
+
+export default Rank;
