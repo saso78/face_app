90e689bc87f265d2f7cc8c85e93f0529dffccb39
saso78 <sasotrpovski@gmail.com>
2018-12-22 00:54:46 +0100

    added all the components , image api functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Clarifai from'clarifai';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
 import './App.css';
+ 
+
+const app = new Clarifai.App({
+ apiKey: 'e1539ecaf18c4b958e63c882f47ad6df'
+});
+ 
+
 const particlesOptions = {
                 particles: {
                   number: {
@@ -22,11 +31,28 @@ class App extends Component {
 		super();
 		this.state ={
 			input:'',
+      imageUrl:''
 		}
 	}
 
-onInputChange = (event) =>{
- console.log(event.target.value);
+onInputChange = (event) => {
+this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+this.setState({imageUrl: this.state.input});  
+  app.models
+  .predict(
+    Clarifai.COLOR_MODEL, 
+    this.state.input)
+  .then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 }
 
   render() {
@@ -39,12 +65,12 @@ onInputChange = (event) =>{
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange}/>
-      { /*
-            
-            <FaceRecognition /> */
-        }
-        
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition imageUrl={this.state.imageUrl}/> 
+              
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css'
 
 
-const ImageLinkForm = ({onInputChange}) => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 return (
   <div >
     <p className='f3'>
@@ -10,8 +10,8 @@ return (
     </p>
     <div className='center'>
       <div className='pa4 br3 shadow-5 center form'>
-    	<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-    	<button className='w-30 grow f4 link f3 ph3 pv2 dib white bg-light-purple'>Detect</button>
+    	<input className='f4 pa2 w-70 center' type='tex' onChange={ onInputChange } />
+    	<button className='w-30 grow f4 link f3 ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
       </div>
     </div>
   </div>
