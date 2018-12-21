92f4510e878d6fa778352b428c818ff846304576
saso78 <sasotrpovski@gmail.com>
2018-12-21 13:58:04 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 const particlesOptions = {
                 particles: {
                   number: {
-                    value:30, 
+                    value:80, 
                     density:{
                       enable:true,
                       value_area:800,
