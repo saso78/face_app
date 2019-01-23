a4e767ad5f5fb9b48ceba390f7efa7415f37486c
saso78 <sasotrpovski@gmail.com>
2019-01-23 12:04:36 +0100

    instalirani potrebni delovi za da raboti

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -3,26 +3,7 @@ import React from 'react';
 
 
 
-class Signin extends React.component {
-
-constructor(props){
-  super(props);
-  this.state={
-    signInEmail:'',
-    signInPassword:''
-  }
-}
-
-  onEmailChange = (event) =>{
-    this.setState({signInEmail:event.target.value})
-  }
-
-   onPasswordChange = (event) =>{
-    this.setState({signInPassword:event.target.value})
-  }
-
-  render() {
-    const { onRouteChange }= this.props ;
+const Signin = ({onRouteChange}) => {
 return (
 <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l shadow-5 mw6 center">
    <main className="pa4 black-80">
@@ -30,11 +11,11 @@ return (
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
       <legend className="f3 fw6 ph0 mh0 center">Sign In</legend>
       <div className="mt3">
-        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
       </div>
       <div className="mv3">
-        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+        <label className="db fw6 lh-copy f6" for="password">Password</label>
         <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
       </div>
      
@@ -53,9 +34,9 @@ return (
   </form>
 </main>
 </article>
-  );
-  }
-
+  )
 }
+
+
 
 export default Signin;
